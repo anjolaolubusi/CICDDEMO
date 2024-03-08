6f0edc9d62d8b7b62e08327b05ae9279bf042e6e
Anjolaoluwa Olubusi <Anjolaoluwa.Olubusi@acadiahealthcare.com>
2024-03-08 11:45:09 -0600

    Fixed Error

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def about():
 
 @app.route("/marco", methods=['GET'])
 def marco():
-    return "Polo1"
+    return "Polo"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
