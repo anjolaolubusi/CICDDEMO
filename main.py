78c5561e2c243d648ce7b9e4670a6188badbe047
Anjolaoluwa Olubusi <Anjolaoluwa.Olubusi@acadiahealthcare.com>
2024-03-08 11:44:01 -0600

    Error Example

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def about():
 
 @app.route("/marco", methods=['GET'])
 def marco():
-    return "Polo"
+    return "Polo1"
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
